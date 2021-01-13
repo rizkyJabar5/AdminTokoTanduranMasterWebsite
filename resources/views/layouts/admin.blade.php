661e4df292d559f578c8888ae8d6d40e370c5645
Rizky Abdul Jabar <rkhamet12@gmail.com>
2021-01-13 16:13:23 +0700

    Function login get userName

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -69,25 +69,27 @@
         <span class="brand-text font-weight-light">Toko Tanduran</span>
       </a>
 
-      <!-- Sidebar -->
-      <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div class="image">
-            <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-          </div>
-          <div class="info">
-            <a href="#" class="d-block">{{get_current_user()}}</a>
-          </div>
-        </div>
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <!-- Sidebar user panel (optional) -->
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                    <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                </div>
+                <div class="info">
+                    <a href="#"
+                       class="d-block">{{{ isset(Auth::user()-> name)? \Illuminate\Support\Facades\Auth::user()->name : \Illuminate\Support\Facades\Auth::user()->email}}}</a>
+                </div>
+            </div>
 
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-            <li class="nav-item">
-              <a href="{{ route('home') }}" class="nav-link">
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                    data-accordion="false">
+                    <!-- Add icons to the links using the .nav-icon class
+                       with font-awesome or any other icon font library -->
+                    <li class="nav-item">
+                        <a href="{{ route('home') }}" class="nav-link">
                 <i class="nav-icon fas fa-th"></i>
                 <p>
                   Dashboard
